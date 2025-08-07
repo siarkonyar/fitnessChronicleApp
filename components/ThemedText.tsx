f23e3bfac15f995af477e0d68fd40e80f3f488b5
siarkonyar <siarkonyar@gmail.com>
2025-08-08 02:47:30 +0300

    some ui fixes

--- components/ThemedText.tsx
+++ components/ThemedText.tsx
@@ -44,12 +44,12 @@ const styles = StyleSheet.create({
     fontWeight: "600",
   },
   title: {
-    fontSize: 32,
+    fontSize: 24,
     fontWeight: "bold",
     lineHeight: 32,
   },
   subtitle: {
-    fontSize: 24,
+    fontSize: 20,
     fontWeight: "bold",
   },
   link: {
