7715837da3152ebb8f15dc37dafb186d3f148ad0
siarkonyar <siarkonyar@gmail.com>
2025-08-10 02:45:01 +0300

    before sideloading

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,7 +1,9 @@
 // Import the functions you need from the SDKs you need
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 import { getAnalytics } from "firebase/analytics";
 import { initializeApp } from "firebase/app";
-import { getAuth } from 'firebase/auth';
+import * as firebaseAuth from 'firebase/auth';
+import { initializeAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -19,9 +21,12 @@ const firebaseConfig = {
   measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
+const reactNativePersistence = (firebaseAuth as any).getReactNativePersistence;
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+export const auth = initializeAuth(app, {
+  persistence: reactNativePersistence(ReactNativeAsyncStorage)
+});
 const analytics = getAnalytics(app);
 export const db = getFirestore(app);
