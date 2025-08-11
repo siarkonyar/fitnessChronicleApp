d846ac8cacd2b47107b14f5f9288456a00bafc87
siarkonyar <siarkonyar@gmail.com>
2025-08-12 01:19:01 +0300

    finished styling edits

--- app/(tabs)/settings/_layout.tsx
+++ app/(tabs)/settings/_layout.tsx
@@ -1,13 +1,16 @@
 import { Stack } from "expo-router";
 import React from "react";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 import { ThemedText } from "@/components/ThemedText";
-import { View } from "react-native";
+import { Colors } from "@/constants/Colors";
+import { View, useColorScheme } from "react-native";
 
 export default function CalendarStackLayout() {
+  const theme = useColorScheme() ?? "light";
+  const insets = useSafeAreaInsets();
   return (
-    <SafeAreaView className="flex-1 flex-col bg-transparent">
+    <View className="flex-1 flex-col bg-transparent">
       {/* Fixed left header */}
       {/* <BlurView intensity={50} tint={"dark"}>
         <View className="px-4 py-3">
@@ -15,8 +18,20 @@ export default function CalendarStackLayout() {
         </View>
       </BlurView> */}
 
-      <View className="px-4 py-3">
-        <ThemedText type="title">Settings</ThemedText>
+      <View
+        className="px-4 py-3"
+        style={{
+          paddingTop: insets.top,
+          backgroundColor: Colors[theme].highlight,
+        }}
+      >
+        <ThemedText
+          lightColor={Colors.light.cardBackground}
+          darkColor={Colors.dark.cardBackground}
+          type="title"
+        >
+          Settings
+        </ThemedText>
       </View>
 
       <Stack
@@ -27,6 +42,6 @@ export default function CalendarStackLayout() {
       >
         <Stack.Screen name="index" options={{ headerShown: false }} />
       </Stack>
-    </SafeAreaView>
+    </View>
   );
 }
