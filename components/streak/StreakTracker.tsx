e7862c0b6efaa5b3bdee17186e7451f25e59b2e8
siarkonyar <siarkonyar@gmail.com>
2026-04-01 23:42:36 +0100

    feat: add streak bar to streak tracker

--- components/streak/StreakTracker.tsx
+++ components/streak/StreakTracker.tsx
@@ -1,24 +1,50 @@
 import { Colors } from "@/constants/Colors";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { FontAwesome6 } from "@expo/vector-icons";
 import React from "react";
-import { useColorScheme } from "react-native";
+import { useColorScheme, View } from "react-native";
 import Card from "../Card";
 import { ThemedText } from "../ThemedText";
 import { ThemedView } from "../ThemedView";
 
 export default function StreakTracker() {
   const theme = useColorScheme() ?? "dark";
-  return (
-    <Card className="flex-row items-center justify-between">
-      <MaterialCommunityIcons
-        name="fire"
-        size={96}
-        color={Colors[theme].highlight}
-        className="bg-white"
-      />
 
-      <ThemedView>
-        <ThemedText>naebr</ThemedText>
+  const streakValue = 3;
+
+  const renderDots = () => {
+    const dots = [];
+    const clampedValue = Math.min(Math.max(streakValue, 0), 4);
+
+    for (let i = 0; i < 4; i++) {
+      const isLit = i <= 4 - clampedValue;
+      dots.push(
+        <FontAwesome6
+          name={`${isLit ? "bolt" : "circle"}`}
+          size={isLit ? 64 : 48}
+          color={Colors[theme].highlight}
+        />,
+      );
+    }
+    return dots;
+  };
+
+  return (
+    <Card>
+      <ThemedView className="items-center flex-row">
+        <ThemedText type="subtitle">
+          Streak{" "}
+          <ThemedText
+            type="subtitle"
+            lightColor={Colors[theme].highlight}
+            darkColor={Colors[theme].highlight}
+          >
+            {streakValue}
+          </ThemedText>
+        </ThemedText>
+      </ThemedView>
+
+      <ThemedView className="flex-col items-center gap-2">
+        <View className="flex-row items-center">{renderDots()}</View>
       </ThemedView>
     </Card>
   );
