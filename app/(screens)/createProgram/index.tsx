7a0fdc8228a92f952640dad8ce2513ba0894de5d
siarkonyar <siarkonyar@gmail.com>
2026-05-28 01:06:18 +0100

    feat: add text input for program name in create program screen

--- app/(screens)/createProgram/index.tsx
+++ app/(screens)/createProgram/index.tsx
@@ -1,5 +1,22 @@
 import { ThemedView } from "@/components/ThemedView";
+import AppTextInput from "@/components/ui/AppTextInput";
+import { useState } from "react";
 
 export default function CreateProgramScreen() {
-  return <ThemedView className="flex-1" />;
+  const [programName, setProgramName] = useState("");
+  return (
+    <>
+      <ThemedView>
+        <AppTextInput
+          value={programName}
+          onChangeText={setProgramName}
+          autoFocus={false}
+          className="w-full text-3xl font-semibold"
+          style={{ textTransform: "uppercase" }}
+          placeholder="Exercise name..."
+          autoCapitalize="characters"
+        />
+      </ThemedView>
+    </>
+  );
 }
