6f04bfec13aaaa8b1c1eb4378556fe92560c4060
siarkonyar <siarkonyar@gmail.com>
2026-06-03 12:01:44 +0100

    feat: add test group to test formatDateAsString

--- lib/__tests__/dateUtils.test.ts
+++ lib/__tests__/dateUtils.test.ts
@@ -1,4 +1,4 @@
-import { findMostRecentSessionDate } from "../dateUtils";
+import { findMostRecentSessionDate, formatDateAsString } from "../dateUtils";
 
 describe("findMostRecentSessionDate", () => {
   test("returns the most recent date when several are before today", () => {
@@ -61,5 +61,40 @@ describe("findMostRecentSessionDate", () => {
     const result = findMostRecentSessionDate(dates, today);
 
     expect(result).toBe("2026-05-30");
+  });
+});
+
+describe("formatDateAsString", () => {
+  beforeEach(() => {
+    jest.useFakeTimers();
+    jest.setSystemTime(new Date("2026-06-01T12:00:00Z"));
+  });
+
+  afterEach(() => {
+    jest.useRealTimers();
+  });
+
+  test("formats a non-today date as 'Nth of Month YYYY'", () => {
+    expect(formatDateAsString("2026-06-15")).toBe("15th of June 2026");
+  });
+
+  test("uses 'st' for the 1st", () => {
+    expect(formatDateAsString("2026-01-01")).toBe("1st of January 2026");
+  });
+
+  test("uses 'nd' for the 2nd", () => {
+    expect(formatDateAsString("2026-01-02")).toBe("2nd of January 2026");
+  });
+
+  test("uses 'rd' for the 3rd", () => {
+    expect(formatDateAsString("2026-01-03")).toBe("3rd of January 2026");
+  });
+
+  test("uses 'th' for the 11th (the teens are special)", () => {
+    expect(formatDateAsString("2026-01-11")).toBe("11th of January 2026");
+  });
+
+  test("uses 'st' again for the 21st", () => {
+    expect(formatDateAsString("2026-01-21")).toBe("21st of January 2026");
   });
 });
