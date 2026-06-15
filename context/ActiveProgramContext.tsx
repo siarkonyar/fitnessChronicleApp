e2c6de0ebb1d2476670f2f94f9c5a1b8e5e8fabb
siarkonyar <siarkonyar@gmail.com>
2026-06-15 13:13:27 +0100

    feat: finalise the ActiveProgramContex

--- context/ActiveProgramContext.tsx
+++ context/ActiveProgramContext.tsx
@@ -1,16 +1,53 @@
-import { ProgramSchema } from "@/types/types";
-import React from "react";
+import { useActiveProgram } from "@/hooks/useActiveProgram";
+import { ProgramWithIdSchema } from "@/types/types";
+import React, { createContext, useContext } from "react";
 import { z } from "zod";
 
-type Program = z.infer<typeof ProgramSchema>;
+type ProgramWithId = z.infer<typeof ProgramWithIdSchema>;
 
 interface ActiveProgramContextType {
-  activeProgram: Program;
+  activeProgram: ProgramWithId | undefined;
+  programDay: number | undefined;
   selectProgram: (programId: string) => void;
   isSelecting: boolean;
   isLoading: boolean;
 }
 
-export default function ActiveProgramContext() {
-  return <div>ProgramContext</div>;
+const ActiveProgramContext = createContext<
+  ActiveProgramContextType | undefined
+>(undefined);
+
+export function ActiveProgramProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const { activeProgram, programDay, selectProgram, isSelecting, isLoading } =
+    useActiveProgram();
+
+  const value: ActiveProgramContextType = {
+    activeProgram,
+    programDay,
+    selectProgram,
+    isSelecting,
+    isLoading,
+  };
+
+  return (
+    <ActiveProgramContext.Provider value={value}>
+      {children}
+    </ActiveProgramContext.Provider>
+  );
+}
+
+export function useActiveProgramContext() {
+  const context = useContext(ActiveProgramContext);
+
+  if (context === undefined) {
+    throw new Error(
+      "useActiveProgramContext must be used within an ActiveProgramProvider",
+    );
+  }
+
+  return context;
 }
